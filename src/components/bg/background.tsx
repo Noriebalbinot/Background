f40f8250b4a2c4fa651ef1f23d7e842af14d8e10
Norie Balbinot <norie12@hotmail.com>
2023-03-27 20:15:54 -0300

    colors

--- src/components/bg/background.tsx
+++ src/components/bg/background.tsx
@@ -4,8 +4,35 @@ import "./background.css";
 export function AnimatedBackground() {
 	const [time, settime] = useState("");
 	const numberformat = (n: number) => (n < 10 ? "0" + n : "" + n);
-
+	const colorst = [
+		"crimson",
+		"steelblue",
+		"#333",
+		"#957D95",
+		"#904c77",
+		"#011638",
+		"#9dacff",
+	];
+	const colors2 = [
+		"crimson",
+		"steelblue",
+		"#333",
+		"#957D95",
+		"#904c77",
+		"#011638",
+		"#9dacff",
+	];
 	useEffect(() => {
+		const time2 = setInterval(() => {
+			document.body.style.setProperty(
+				"--color1",
+				colorst[Math.floor(Math.random() * 6) + 1]
+			);
+			document.body.style.setProperty(
+				"--color2",
+				colors2[Math.floor(Math.random() * 6) + 1]
+			);
+		}, 10000);
 		const settimeout = setInterval(() => {
 			const date = new Date();
 			settime(
@@ -17,8 +44,14 @@ export function AnimatedBackground() {
 			);
 		}, 1000);
 		return () => {
+			clearTimeout(time2);
 			clearTimeout(settimeout);
 		};
 	}, []);
-	return <div className="AnimatedBackground">{time}</div>;
+	return (
+		<div className="AnimatedBackground">
+			<div className="hours"></div>
+			{time}
+		</div>
+	);
 }
