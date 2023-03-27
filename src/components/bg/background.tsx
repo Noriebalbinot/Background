836ddab15f7ae97c3ffa23eb848e834a5fda6c3c
Norie Balbinot <norie12@hotmail.com>
2023-03-27 10:37:50 -0300

    time

--- src/components/bg/background.tsx
+++ src/components/bg/background.tsx
@@ -1,14 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./background.css";
 
 export function AnimatedBackground() {
+	const [time, settime] = useState("");
+	const numberformat = (n: number) => (n < 10 ? "0" + n : "" + n);
+
 	useEffect(() => {
-		const setbounds = (e: MouseEvent) => {
-			document.body.style.setProperty("--x", e.clientX + "px");
-			document.body.style.setProperty("--y", e.clientY + "px");
+		const settimeout = setInterval(() => {
+			const date = new Date();
+			settime(
+				numberformat(date.getHours()) +
+					":" +
+					numberformat(date.getMinutes()) +
+					":" +
+					numberformat(date.getSeconds())
+			);
+		}, 1000);
+		return () => {
+			clearTimeout(settimeout);
 		};
-		window.addEventListener("mousemove", setbounds);
-		return () => window.removeEventListener("mousemove", setbounds);
 	}, []);
-	return <div className="AnimatedBackground"></div>;
+	return <div className="AnimatedBackground">{time}</div>;
 }
